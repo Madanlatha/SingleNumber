22a91a4b1372bcd7ccc109380c1d7b3be926a58a
Madanlatha <madanganesan08@gmail.com>
2023-07-18 13:05:10 +0530

    First commit

---
+++ Snumber.java
@@ -0,0 +1,29 @@
+package singleNumber;
+
+import java.util.Scanner;
+
+public class Snumber {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int ar[]=new int[n];
+		for(int i=0;i<n;i++) {
+			ar[i]=sc.nextInt();
+		}
+		int vals=Singlenumber(ar);
+		System.out.println(vals);
+	}
+
+
+	        public static int Singlenumber(int ar[]){
+	            int result=0;
+	            for(int i=0;i<ar.length;i++){
+	                result=result^ar[i];
+
+	            }
+	            return result;
+	        }
+	    
+	}
+
+
